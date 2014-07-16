b53912b24bf2a29bfeffd82b116f0e989fc4c9c7
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-07-16 15:06:09 +0200

    no message

---
+++ La Andaluza/Control/Procesos abiertos/frmProceso.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmProceso
+
+End Class
---
+++ La Andaluza/Control/Procesos abiertos/frmProcesosAbiertos.vb
@@ -0,0 +1,30 @@
+﻿Public Class frmProcesosAbiertos
+
+    Public Sub New()
+
+        InitializeComponent()
+
+
+        Me.SuspendLayout()
+        For i As Integer = 0 To 10
+
+            Dim frm As New frmTrasiego
+            frm.WindowState = FormWindowState.Normal
+            frm.TopLevel = False
+            frm.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+            frm.Dock = DockStyle.None
+            frm.Show()            
+            Me.FlowLayoutPanel1.Controls.Add(frm)
+            Me.Refresh()
+        Next
+        Me.ResumeLayout()
+    End Sub
+
+    Private Sub tsCompacto_Click(sender As Object, e As EventArgs) Handles tsCompacto.Click
+        Me.FlowLayoutPanel1.Visible = False
+    End Sub
+
+    Private Sub tsDetallado_Click(sender As Object, e As EventArgs) Handles tsDetallado.Click
+        Me.FlowLayoutPanel1.Visible = True
+    End Sub
+End Class
