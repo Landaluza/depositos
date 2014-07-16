34333019057cf8cef5a4b1ecd3cac2d93be54b6e
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-07-16 09:17:41 +0200

    no message

--- La Andaluza/Bases/Conf/Config.vb
+++ La Andaluza/Bases/Conf/Config.vb
@@ -31,7 +31,7 @@
     'Public Shared PrintPreviewDialogEtiquetas As PrintPreviewDialog
 
     Public Shared Sub Cargar_Ajustes_Predeterminados()
-        Config.Server = DataBase.LOCAL
+        Config.Server = DataBase.SERVIDOR
         Config.MailReportPass = "Administracion2008"
         Config.HelpUrl = "http://192.168.1.106/AyudaLA/index.php"
         Config.ventasPath = "Z:\Informatica\La Andaluza app\ExcelFile\Book1.xlsx"
