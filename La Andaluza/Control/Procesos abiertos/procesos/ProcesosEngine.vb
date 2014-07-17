02df03819d75ea072faeb446d22f1e9b82d1f58e
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-07-17 13:46:49 +0200

    no message

--- La Andaluza/Control/Procesos abiertos/procesos/ProcesosEngine.vb
+++ La Andaluza/Control/Procesos abiertos/procesos/ProcesosEngine.vb
@@ -1,16 +1,16 @@
 ﻿Public Class ProcesosEngine
     Protected id As Integer
+    Private sp As spProcesosAbiertos
     Public Sub New(ByVal id As Integer)
         Me.id = id
+        sp = New spProcesosAbiertos
     End Sub
 
     Public Function borrar_proceso_abierto() As Boolean
-        Dim sp As New spProcesosAbiertos
         Return sp.borrar(id)
     End Function
 
     Public Function seleccionar() As DataTable
-        Dim sp As New spProcesosAbiertos
         Return sp.seleccionar(id)
     End Function
 End Class
