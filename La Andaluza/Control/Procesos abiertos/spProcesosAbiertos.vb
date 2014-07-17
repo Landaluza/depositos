4acf0b103dd808b11d915cde9431f98b923f4769
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-07-17 14:02:58 +0200

    no message

--- La Andaluza/Control/Procesos abiertos/spProcesosAbiertos.vb
+++ La Andaluza/Control/Procesos abiertos/spProcesosAbiertos.vb
@@ -5,10 +5,10 @@
         MyBase.New(Config.Server)
     End Sub
     Public Function devolverProcesosAbiertos() As DataTable
-        Return Me.Consultar("select MovimientoID,Fecha,procesos.Descripcion + ' de ' + Cantidad +' litros del deposito ' +dep.codigo + ' al deposito ' + dep2.codigo,LoteID,FiltroID " & _
+        Return Me.Consultar("MovimientoID,Fecha,procesos.Descripcion + ' de ' + convert(varchar, Cantidad) +' litros del deposito ' + convert(varchar,dep.codigo) + ' al deposito ' + convert(varchar,dep2.codigo) " & _
                             "from movimientosAbiertos, procesos, depositos dep, depositos dep2 " & _
                             "where movimientosAbiertos.procesoid = procesos.ProcesoID " & _
-                            "and movimientosAbiertos.EntraDepositoID = dep2.DepositoI  " & _
+                            "and movimientosAbiertos.EntraDepositoID = dep2.DepositoID  " & _
                             "and movimientosAbiertos.SaleDepositoID  = dep.DepositoID", False)
     End Function
     
@@ -16,7 +16,7 @@
     Public Function añadirProceso(ByVal proceso As Integer) As Integer
         Me.EmpezarTransaccion()
         Try
-            If Not Me.ConsultaAlteraciones("insert into MovimientosAbiertos(procesoid) values(" & proceso & ")") Then
+            If Not Me.ConsultaAlteraciones("insert into MovimientosAbiertos(procesoid, fecha) values(" & proceso & ", CURRENT_TIMESTAMP)") Then
                 Me.CancelarTransaccion()
                 Return 0
             End If
