8673b8ab358119acd40bbe9a0db481fea0d4712a
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-09-26 14:00:44 +0200

    no message

---
+++ La Andaluza/Elaboracion/Movimientos/Diferencias/GuiDiferencia.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GuiDiferencia
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(GuiDiferencia))
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.dtpFecha = New System.Windows.Forms.DateTimePicker()
+        Me.dgvDestino = New System.Windows.Forms.DataGridView()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnProductoDestinoIncorrecto = New System.Windows.Forms.Button()
+        Me.btncantidadDestinoIncorrecta = New System.Windows.Forms.Button()
+        Me.lDescripcionDestino = New System.Windows.Forms.Label()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        CType(Me.dgvDestino, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.dtpFecha)
+        Me.FlowLayoutPanel1.Controls.Add(Me.dgvDestino)
+        Me.FlowLayoutPanel1.Controls.Add(Me.GroupBox2)
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(83, 22)
+        Me.FlowLayoutPanel1.Margin = New System.Windows.Forms.Padding(25, 3, 3, 3)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(577, 460)
+        Me.FlowLayoutPanel1.TabIndex = 36
+        '
+        'dtpFecha
+        '
+        Me.dtpFecha.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFecha.Location = New System.Drawing.Point(468, 3)
+        Me.dtpFecha.Name = "dtpFecha"
+        Me.dtpFecha.Size = New System.Drawing.Size(106, 20)
+        Me.dtpFecha.TabIndex = 32
+        '
+        'dgvDestino
+        '
+        Me.dgvDestino.AllowUserToAddRows = False
+        Me.dgvDestino.AllowUserToDeleteRows = False
+        Me.dgvDestino.AllowUserToResizeRows = False
+        Me.dgvDestino.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.dgvDestino.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        Me.dgvDestino.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDestino.Location = New System.Drawing.Point(3, 29)
+        Me.dgvDestino.Name = "dgvDestino"
+        Me.dgvDestino.ReadOnly = True
+        Me.dgvDestino.RowHeadersVisible = False
+        Me.dgvDestino.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvDestino.Size = New System.Drawing.Size(571, 239)
+        Me.dgvDestino.TabIndex = 15
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnProductoDestinoIncorrecto)
+        Me.GroupBox2.Controls.Add(Me.btncantidadDestinoIncorrecta)
+        Me.GroupBox2.Controls.Add(Me.lDescripcionDestino)
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 274)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(571, 165)
+        Me.GroupBox2.TabIndex = 18
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Detalles"
+        '
+        'btnProductoDestinoIncorrecto
+        '
+        Me.btnProductoDestinoIncorrecto.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btnProductoDestinoIncorrecto.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnProductoDestinoIncorrecto.Image = CType(resources.GetObject("btnProductoDestinoIncorrecto.Image"), System.Drawing.Image)
+        Me.btnProductoDestinoIncorrecto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnProductoDestinoIncorrecto.Location = New System.Drawing.Point(3, 91)
+        Me.btnProductoDestinoIncorrecto.Name = "btnProductoDestinoIncorrecto"
+        Me.btnProductoDestinoIncorrecto.Size = New System.Drawing.Size(565, 37)
+        Me.btnProductoDestinoIncorrecto.TabIndex = 4
+        Me.btnProductoDestinoIncorrecto.Text = "Producto incorrecto"
+        Me.btnProductoDestinoIncorrecto.UseVisualStyleBackColor = True
+        '
+        'btncantidadDestinoIncorrecta
+        '
+        Me.btncantidadDestinoIncorrecta.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btncantidadDestinoIncorrecta.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btncantidadDestinoIncorrecta.Image = CType(resources.GetObject("btncantidadDestinoIncorrecta.Image"), System.Drawing.Image)
+        Me.btncantidadDestinoIncorrecta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btncantidadDestinoIncorrecta.Location = New System.Drawing.Point(3, 128)
+        Me.btncantidadDestinoIncorrecta.Name = "btncantidadDestinoIncorrecta"
+        Me.btncantidadDestinoIncorrecta.Size = New System.Drawing.Size(565, 34)
+        Me.btncantidadDestinoIncorrecta.TabIndex = 3
+        Me.btncantidadDestinoIncorrecta.Text = "Cantidad incorrecta"
+        Me.btncantidadDestinoIncorrecta.UseVisualStyleBackColor = True
+        '
+        'lDescripcionDestino
+        '
+        Me.lDescripcionDestino.Dock = System.Windows.Forms.DockStyle.Top
+        Me.lDescripcionDestino.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lDescripcionDestino.Location = New System.Drawing.Point(3, 16)
+        Me.lDescripcionDestino.Name = "lDescripcionDestino"
+        Me.lDescripcionDestino.Size = New System.Drawing.Size(565, 72)
+        Me.lDescripcionDestino.TabIndex = 1
+        Me.lDescripcionDestino.Text = "-"
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGuardar.Image = Global.La_Andaluza_MovimientosDepositos.My.Resources.Resources.glyphicons_198_ok
+        Me.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnGuardar.Location = New System.Drawing.Point(416, 488)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(244, 69)
+        Me.btnGuardar.TabIndex = 35
+        Me.btnGuardar.Text = "Continuar"
+        Me.btnGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtCantidad.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte), True)
+        Me.txtCantidad.Location = New System.Drawing.Point(83, 488)
+        Me.txtCantidad.Multiline = True
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(210, 69)
+        Me.txtCantidad.TabIndex = 34
+        Me.txtCantidad.Text = "0"
+        Me.txtCantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GuiDiferencia
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(744, 591)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Name = "GuiDiferencia"
+        Me.Text = "GuiDiferencia"
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        CType(Me.dgvDestino, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents FlowLayoutPanel1 As System.Windows.Forms.FlowLayoutPanel
+    Private WithEvents dtpFecha As System.Windows.Forms.DateTimePicker
+    Public WithEvents dgvDestino As System.Windows.Forms.DataGridView
+    Private WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Private WithEvents btnProductoDestinoIncorrecto As System.Windows.Forms.Button
+    Private WithEvents btncantidadDestinoIncorrecta As System.Windows.Forms.Button
+    Friend WithEvents lDescripcionDestino As System.Windows.Forms.Label
+    Public WithEvents btnGuardar As System.Windows.Forms.Button
+    Public WithEvents txtCantidad As System.Windows.Forms.TextBox
+End Class
---
+++ La Andaluza/Elaboracion/Movimientos/Diferencias/GuiDiferencia.vb
@@ -0,0 +1,3 @@
+﻿Public Class GuiDiferencia
+
+End Class
